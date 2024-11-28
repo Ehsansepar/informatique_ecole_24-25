89afa649cd9963e0363d60bdf770c3fad8b5a8f0
Ehsansepar <ehsan.separ007@gmail.com>
2024-11-28 16:41:00 +0100

    Exercise 2,3 DONE

--- fonction.py
+++ fonction.py
@@ -3,7 +3,7 @@
 
 # --- Définition des fonctions ---
 
-# ex 1
+# Exercice 1
 
 def somme(liste) :
     total = 0 
@@ -15,6 +15,42 @@ def somme(liste) :
     for nbr in range(len(liste)) :
         total += liste[nbr]
     return total
+# -----------------------------------------------------------------------------
+
+# Exercice 2
+
+def pair(nbr) :
+    if nbr % 2 == 0 :
+        return True 
+        ...
+    elif nbr % 2 == 1 : 
+        return False
+
+# -----------------------------------------------------------------------------
+
+# Exercice 3
+
+def maximum(n1, n2, n3) :
+    max = 0
+
+    # if n1 < n2 :
+    #     return n2
+    # elif n2 < n3 :
+    #     return n3
+    # else :
+    #     return n1
+
+    lst = [n1, n2, n3]
+
+    maxi = 0
+
+    for i in range(len(lst)) : 
+        if lst[i] > maxi :
+            maxi = lst[i]
+    
+    return maxi
+
+# -----------------------------------------------------------------------------
 
 if __name__ == "__main__":
     # Example 1
@@ -23,5 +59,12 @@ if __name__ == "__main__":
     
     # ------------------------------------------
 
+    # Example 2 
+    # print(pair(3)) # retournera 0
+    # print(pair(4)) # retournera 1
+    
+    # ------------------------------------------
 
+    # Example 3 
+    # print(maximum(2, 5, 4)) # retournera 5
     ...
